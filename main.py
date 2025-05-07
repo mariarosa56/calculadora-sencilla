41993d64bd594e7e40deaff12bdb56b063c2c11d
Blancaroblesluna <blanrona07@gmail.com>
2025-05-07 21:42:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+def sumar(a, b):
+    return a + b
+
+#no se si a alguien le sale esto pero definir aquí arriba cada una de vuestras funciones
+
+def main():
+    print("Calculadora en Python")
+    print("1. Sumar")
+    print("2. Restar")  
+    print("3. Multiplicar")
+    print("4. Dividir")
+
+  opcion = int(input("Selecciona una opción (1/2/3/4): "))
+  num1 = float(input("Ingresa el primer número: "))
+  num2 = Float(input("Ingresa el segundo número: "))
+
+  if opcion == 1
+    print("Resultado:", sumar(num1, num2)")
+  #añadir otras opciones de la calculadora
+
+   else:
+    print("Syntax error")
+
+if __name__ == "__main__":
+    main()
