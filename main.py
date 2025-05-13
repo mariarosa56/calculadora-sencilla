c867469d2e016bf2f111da2aae87ea1b781d36a1
Sofia1662 <sofiuchi7@gmail.com>
2025-05-13 18:50:32 +0000

    interfaz de menu

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def restar(a, b):
     return a - b
 def multiplicar(a, b):
     return a * b
-
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else: print("Syntax error")
+      
 #no se si a alguien le sale esto pero definir aquí arriba cada una de vuestras funciones
 
 def main():
@@ -13,10 +17,11 @@ def main():
     print("2. Restar")  
     print("3. Multiplicar")
     print("4. Dividir")
-
+    print("5. Salir")
+ 
   opcion = int(input("Selecciona una opción (1/2/3/4): "))
   num1 = float(input("Ingresa el primer número: "))
-  num2 = Float(input("Ingresa el segundo número: "))
+  num2 = float(input("Ingresa el segundo número: "))
 
   if opcion == 1
     print("Resultado:", sumar(num1, num2)")
@@ -26,6 +31,9 @@ def main():
 
   elif opcion == 3
     print("Resultado:", multiplicar(num1, num2)")
+
+  elif opcion == 4
+    print("Resultado:", dividir(num1, num2)")
           
                 
   #añadir otras opciones de la calculadora
